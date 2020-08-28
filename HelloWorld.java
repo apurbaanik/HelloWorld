ad905f2ee01c790356e6ed29b0ee950154dc84b7
Anik Barua <apurbaanik@gmail.com>
2020-08-28 11:20:26 -0400

    Adding the source code for HelloWorld

---
+++ HelloWorld.java
@@ -0,0 +1,17 @@
+/**
+ * @author Anik Barua
+ * @since 08-28-2020
+ * @version 1.0
+ * 
+ * Description: Lab #1 Setup.
+ */
+
+
+public class HelloWorld {
+    
+    public static void main(String[]args) {
+
+      System.out.println("Hello World!");
+
+    }
+}
