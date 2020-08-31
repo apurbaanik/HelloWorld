2c020fd8b77a6355628019ce239f5d9c2471c0dd
Anik Barua <apurbaanik@gmail.com>
2020-08-31 19:57:25 -0400

    Added comments in the source code

--- HelloWorld.java
+++ HelloWorld.java
@@ -6,11 +6,9 @@
  * Description: Lab #1 Setup - This java program prints out Hello World in the output. 
  */
 
-
 public class HelloWorld {
-    // Main method
-    public static void main(String[]args) {
-      System.out.println("Hello World!"); // Prints out "Hello World! in the output tab."
+    public static void main(String[]args) {  // Main method
+      System.out.println("Hello World!"); // Prints out "Hello World! in the output tab.
     } // End of main method
 
 } // End of HelloWorld class
