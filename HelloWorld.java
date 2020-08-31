fbd88538b4d8d4e9ec4c6672a95d0992926ade1c
Anik Barua <apurbaanik@gmail.com>
2020-08-31 11:19:03 -0400

    Added comments in my source code

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,17 +1,16 @@
 /**
  * @author Anik Barua
- * @since 08-30-2020
+ * @since 08-31-2020
  * @version 1.0
  * 
- * Description: Lab #1 Setup.
+ * Description: Lab #1 Setup - This java program prints out Hello World in the output. 
  */
 
 
 public class HelloWorld {
-    
+    // Main method
     public static void main(String[]args) {
+      System.out.println("Hello World!"); // Prints out "Hello World! in the output tab."
+    } // End of main method
 
-      System.out.println("Hello World!");
-
-    }
-}
+} // End of HelloWorld class
